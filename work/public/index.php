609003218522333b5135caff0047fbb2c4385587
mkida0808 <mkida0808@yahoo.co.jp>
2022-07-29 01:51:37 +0900

    MySQLデータベース接続設定の修正

--- work/public/index.php
+++ work/public/index.php
@@ -1,6 +1,6 @@
 <?php
 
-define('DSN', 'mysql:host=db;dbname=myapp;charset=utf8mb4');
+define('DSN', 'mysql:host=db_dotinstall;dbname=myapp;charset=utf8mb4');
 define('DB_USER', 'myappuser');
 define('DB_PASS', 'myapppass');
 
