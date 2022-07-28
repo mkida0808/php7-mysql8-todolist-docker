37f823bd1f7c6aad624d047416f97e2c14b231c5
mkida0808 <mkida0808@yahoo.co.jp>
2022-07-29 03:41:02 +0900

    Todoリスト一覧に新規タイトル（DBレコード）を追加

--- work/public/index.php
+++ work/public/index.php
@@ -25,11 +25,27 @@ function h($str) {
   return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
 }
 
+// Todoリスト一覧に新規レコードを追加
+function addTodo($pdo) {
+  // 入力フォームから入力タイトルを取得する
+  $title = trim(filter_input(INPUT_POST, 'title'));
+  if ($title === '') return;
+
+  $stmt = $pdo->prepare("INSERT INTO todos (title) VALUES (:title)");
+  $stmt->bindValue('title', $title, PDO::PARAM_STR);
+  $stmt->execute();
+}
+
 // Todoリスト一覧を取得
 function getTodos($pdo) {
   $stmt = $pdo->query("select * from todos order by id desc");
   $todos = $stmt->fetchAll();
   return $todos;
+}
+
+// Todiリスト一覧にタイトルを追加関数を呼び出す
+if ($_SERVER['REQUEST_METHOD'] === "POST") {
+  addTodo($pdo);
 }
 
 // Todoリスト一覧を呼び出す
