c0ddb5de0b789b4ccb11c2a4ae191d382ade9220
mkida0808 <mkida0808@yahoo.co.jp>
2022-07-29 14:01:20 +0900

    config.phpを作成してDB接続情報とセッションを別ファイルに移行

---
+++ work/app/config.php
@@ -0,0 +1,9 @@
+<?php
+
+session_start();
+
+define('DSN', 'mysql:host=db_dotinstall;dbname=myapp;charset=utf8mb4');
+define('DB_USER', 'myappuser');
+define('DB_PASS', 'myapppass');
+// サーバー変数からサイトURL（ドメイン）を取得、定義する
+define('SITE_URL', 'http://' . $_SERVER['HTTP_HOST']);
