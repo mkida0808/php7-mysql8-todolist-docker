cfa92d079817e919e0c6eeeac75ad92266671ef5
mkida0808 <mkida0808@yahoo.co.jp>
2022-07-30 06:03:08 +0900

    htmlspecialchar関数をUtilsクラスに移行、それに伴う実装修正

---
+++ work/app/Utils.php
@@ -0,0 +1,10 @@
+<?php
+
+class Utils
+{
+  // htmlspecialcharsを導入
+  public static function h($str)
+  {
+    return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
+  }
+}
--- work/app/config.php
+++ work/app/config.php
@@ -8,4 +8,5 @@ define('DB_PASS', 'myapppass');
 // サーバー変数からサイトURL（ドメイン）を取得、定義する
 define('SITE_URL', 'http://' . $_SERVER['HTTP_HOST']);
 
+require_once(__DIR__ . '/Utils.php');
 require_once(__DIR__ . '/functions.php');
