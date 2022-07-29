9506c97d76e8e36545410fc7265544f4cb701de2
mkida0808 <mkida0808@yahoo.co.jp>
2022-07-29 14:11:31 +0900

    PDOインスタンスをfunctionsに移行、indexから関数呼び出し

--- work/public/index.php
+++ work/public/index.php
@@ -4,58 +4,7 @@ require_once(__DIR__ . '/../app/config.php');
 
 createToken();
 
-try {
-  $pdo = new PDO(
-    DSN,
-    DB_USER,
-    DB_PASS,
-    [
-      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
-      PDO::ATTR_EMULATE_PREPARES => false,
-    ]
-  );
-} catch (PDOException $e) {
-  echo $e->getMessage();
-  exit;
-}
-
-// htmlspecialcharsを導入
-function h($str) {
-  return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
-}
-
-// ランダム文字列でトークンを作成
-function createToken() {
-  if (!isset($_SESSION['token'])) {
-    $_SESSION['token'] = bin2hex(random_bytes(32));
-  }
-}
-
-// トークンのバリデーション検証
-function validateToken() {
-  if (empty($_SESSION['token']) || $_SESSION['token'] !== filter_input(INPUT_POST, 'token')) {
-    exit('Invalid post request');
-  }
-}
-
-// Todoリスト一覧に新規レコードを追加
-function addTodo($pdo) {
-  // 入力フォームから入力タイトルを取得する
-  $title = trim(filter_input(INPUT_POST, 'title'));
-  if ($title === '') return;
-
-  $stmt = $pdo->prepare("INSERT INTO todos (title) VALUES (:title)");
-  $stmt->bindValue('title', $title, PDO::PARAM_STR);
-  $stmt->execute();
-}
-
-// Todoリスト一覧を取得
-function getTodos($pdo) {
-  $stmt = $pdo->query("select * from todos order by id desc");
-  $todos = $stmt->fetchAll();
-  return $todos;
-}
+$pdo = getPdoInstance();
 
 // Todiリスト一覧にタイトルを追加関数を呼び出す
 if ($_SERVER['REQUEST_METHOD'] === "POST") {
