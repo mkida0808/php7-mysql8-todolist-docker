125f24f399947ba32dd98cbcefac409a1ec7d3ea
mkida0808 <mkida0808@yahoo.co.jp>
2022-07-30 16:44:03 +0900

    config上でspl_autoload_registerでクラスを自動呼び出しに修正

--- work/app/config.php
+++ work/app/config.php
@@ -8,7 +8,14 @@ define('DB_PASS', 'myapppass');
 // サーバー変数からサイトURL（ドメイン）を取得、定義する
 define('SITE_URL', 'http://' . $_SERVER['HTTP_HOST']);
 
-require_once(__DIR__ . '/Utils.php');
-require_once(__DIR__ . '/Token.php');
-require_once(__DIR__ . '/Database.php');
-require_once(__DIR__ . '/Todo.php');
+// 各クラスを自動で呼び出す
+spl_autoload_register(function ($class) {
+  $fileName = sprintf(__DIR__ . '/%s.php', $class);
+
+  if (file_exists($fileName)) {
+    require($fileName);
+  } else {
+    echo 'File not found: ' . $fileName;
+    exit;
+  }
+});
