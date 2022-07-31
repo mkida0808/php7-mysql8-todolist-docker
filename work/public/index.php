4e080ab9644b2a88d7e6a04bb7d7937226e27bde
mkida0808 <mkida0808@yahoo.co.jp>
2022-07-31 19:54:14 +0900

    一括削除のトークン送信をcheckboxのカスタムデータ属性に置き換え、その他CSS微修正

--- work/public/index.php
+++ work/public/index.php
@@ -27,12 +27,8 @@ $todos = $todo->getAll();
   <main>
     <header>
       <h1>Todos</h1>
-
       <!-- Todoリスト一覧の一括削除ボタン（チェックボックスにチェックがあるリスト） -->
-      <form action="?action=purge" method="post">
-        <span class="purge">Purge</span>
-        <input type="hidden" name="token" value="<?= Utils::h($_SESSION['token']); ?>">
-      </form>
+      <span class="purge" data-token="<?= Utils::h($_SESSION['token']); ?>">Purge</span>
     </header>
 
     <form action="?action=add" method="post">
