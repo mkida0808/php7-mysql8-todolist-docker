0f2ca6c530d74d851b652dabe3c9c489b79e5613
mkida0808 <mkida0808@yahoo.co.jp>
2022-08-01 05:24:54 +0900

    Todoリストタイトルの更新を行う時、該当のidレコードが存在するかどうかを調べる処理を追記（JS側で通信エラー時を考慮する）

--- work/app/Todo.php
+++ work/app/Todo.php
@@ -49,7 +49,19 @@ class todo
   private function toggle()
   {
     $id = filter_input(INPUT_POST, 'id');
-    if (empty($id)) return;
+    if (empty($id)) {
+      return;
+    }
+
+    // 該当idのレコードが存在するかどうかを調べる
+    $stmt = $this->pdo->prepare("SELECT * FROM todos WHERE id = :id");
+    $stmt->bindValue('id', $id, \PDO::PARAM_INT);
+    $stmt->execute();
+    $todo = $stmt->fetch();
+    if (empty($todo)) {
+      header('HTTP', true, 404);
+      exit;
+    }
 
     $stmt = $this->pdo->prepare("UPDATE todos SET is_done = NOT is_done WHERE id = :id");
     $stmt->bindValue('id', $id, \PDO::PARAM_INT);
