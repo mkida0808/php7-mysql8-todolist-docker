f83b6512c89f1ca67bf6a71506bf070bea1a0c2f
mkida0808 <mkida0808@yahoo.co.jp>
2022-08-03 20:38:30 +0900

    郵便番頭を正規表現でチェックする挙動確認

--- work/public/udemy/php_mysql_tomosuta/sample22/index.php
+++ work/public/udemy/php_mysql_tomosuta/sample22/index.php
@@ -11,3 +11,13 @@ if (is_numeric($age))
 } else {
   echo '数値を入力して下さい';
 }
+
+echo '<br>';
+
+$zip = '423-4467';
+
+if (preg_match("/\A\d{3}[-]\d{4}\z/", $zip)) {
+  echo '郵便番号：〒' . $zip;
+} else {
+  echo '郵便番号を正しく入力して下さい';
+}
