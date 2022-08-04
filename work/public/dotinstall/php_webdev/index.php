1c9b1161814fd49da26dade7e7b1ba14d1dffd53
mkida0808 <mkida0808@yahoo.co.jp>
2022-08-04 15:50:25 +0900

    配列の要素取得の挙動確認

--- work/public/dotinstall/php_webdev/index.php
+++ work/public/dotinstall/php_webdev/index.php
@@ -1,16 +1,30 @@
 <?php
 require(__DIR__ . '/../../../app/dotinstall/php_webdev/functions.php');
 // $path = __DIR__;
-  $name = 'Makoto, <script>alert(1)</script>, aaaaaa';
+$names = [
+  'Taro',
+  'Jiro',
+  'Saburo',
+];
 ?>
 
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
   <meta charset="UTF-8">
   <title>php web development</title>
 </head>
+
 <body>
+  <?php if (empty($names)) : ?>
+    <p>配列には空です</p>
+  <?php else : ?>
+    <?php foreach ($names as $name) : ?>
+      <li><?php echo h($name); ?></li>
+    <?php endforeach; ?>
+  <?php endif; ?>
   <p>Hello, <?= h($name); ?></p>
 </body>
+
 </html>
