9d3e06c8919fe348bd5e8b9006f73d97863254b0
mkida0808 <mkida0808@yahoo.co.jp>
2022-08-06 13:47:27 +0900

    厳密な型チェック宣言、変数の型付け

--- work/public/dotinstall/php_object/index.php
+++ work/public/dotinstall/php_object/index.php
@@ -1,13 +1,16 @@
 <?php
 
+// 厳密な型チェック
+declare(strict_types=1);
+
 // クラス
 class Post
 {
   // プロパティ
-  public $text;
-  public $likes = 0;
+  private $text;
+  private $likes = 0;
 
-  public function __construct($text)
+  public function __construct(string $text)
   {
     $this->text = $text;
   }
@@ -17,9 +20,22 @@ class Post
   {
     printf('%s (%d)' . PHP_EOL, $this->text, $this->likes);
   }
+
+  public function like()
+  {
+    $this->likes++;
+    if ($this->likes > 100)
+    {
+      $this->likes = 100;
+    }
+  }
 }
 
-$post[0] = new Post('hello'); // インスタンス
-$post[0]->show();
-$post[1] = new Post('hello again'); // インスタンス
-$post[1]->show();
+$posts[0] = new Post('123123123'); // インスタンス
+$posts[1] = new Post('hello again'); // インスタンス
+
+$posts[0]->like();
+$posts[1]->like();
+
+$posts[0]->show();
+$posts[1]->show();
