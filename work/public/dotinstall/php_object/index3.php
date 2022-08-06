7b69cb53aa68c0aff0b02eef96c4875db62d8abc
mkida0808 <mkida0808@yahoo.co.jp>
2022-08-06 20:41:26 +0900

    抽象クラス、抽象メソッドの挙動確認

---
+++ work/public/dotinstall/php_object/index3.php
@@ -0,0 +1,69 @@
+<?php
+
+// 抽象クラス（子クラスに以下のメソッドを必ず明記する）
+abstract class BasePost
+{
+  // private $text;
+  protected $text; // オーバーライドを許す
+
+  public function __construct($text)
+  {
+    $this->text = $text;
+  }
+  // 抽象メソッド
+  abstract public function show();
+}
+
+class Post extends BasePost // 親クラス（スーパークラス）
+{
+  // final修飾子はメソッドのオーバーライドを禁止する
+  // final public function show()
+  public function show()
+  {
+    printf('%s' . PHP_EOL, $this->text);
+  }
+}
+
+class SponsoredPost extends BasePost // 子クラス（サブクラス）
+{
+  private $sponsor;
+
+  public function __construct($text, $sponsor)
+  {
+    parent::__construct($text);
+    $this->sponsor = $sponsor;
+  }
+
+  public function showSponsor()
+  {
+    printf('%s' . PHP_EOL, $this->sponsor);
+  }
+
+  // メソッドのオーバーライド
+  public function show()
+  {
+    printf('%s by %s' . PHP_EOL, $this->text ,$this->sponsor);
+  }
+}
+
+$posts = [];
+$posts[0] = new Post('text000');
+$posts[1] = new Post('text001');
+$posts[2] = new SponsoredPost('texttext', 'sponsorsponsor');
+
+// オブジェクト型の継承（PostだけではなくsponsoredPostも継承する）
+function processPost(BasePost $post)
+{
+  $post->show();
+}
+
+// $posts[0]->show();
+// $posts[1]->show();
+// $posts[2]->show();
+// $posts[2]->showSponsor();
+
+// Post型の表示
+foreach ($posts as $post)
+{
+  processPost($post);
+}
